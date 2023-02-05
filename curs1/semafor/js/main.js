478c6eb1041ff77d9fcd59424a94ee1f20711992
raduiln <117358174+raduiln@users.noreply.github.com>
2023-02-05 21:04:40 +0200

    curs2

--- curs1/semafor/js/main.js
+++ curs1/semafor/js/main.js
@@ -1,29 +1,30 @@
-let lights = document.querySelectorAll('#isiqfor>div');
+let lights = document.querySelectorAll("#isiqfor>div");
 let timers = [500, 2000, 7000];
 let counter = 0;
-let light2 = document.querySelectorAll('#isiqfor2 > div');
-let timer2 = [500, 2000, 7000];
+let lights2 = document.querySelectorAll("#isiqfor2 > div");
+let timers2 = [500, 2000, 7000];
 let counter2 = 2;
 
-window.addEventListener('load', start);
-
-let start = setTimeout(function(){
-    traffic();
-    traffic2();
+let start = setTimeout(function () {
+  traffic();
+  traffic2();
 }, 5000);
+
+window.addEventListener("load", start);
+
 function traffic() {
-    for (i=0; i<lights.length; i++) {
-        light[i].classList.remove('on');
-    }
-        setTimeout(traffic, timers[counter]);
-        counter = (counter + 1) % timers.length;
-    }
-    function traffic2() {
-        for (i=0; i<lights.length; i++) {
-            light[i].classList.remove('on');
-        }
-
-            setTimeout(traffic, timers[counter]);
-            counter = (counter + 1) % timers.length;
-        }
-
+  for (i = 0; i < lights.length; i++) {
+    lights[i].classList.remove("on");
+  }
+  lights[counter].classList.add("on");
+  setTimeout(traffic, timers[counter]);
+  counter = (counter + 1) % timers.length;
+}
+function traffic2() {
+  for (i = 0; i < lights2.length; i++) {
+    lights2[i].classList.remove("on");
+  }
+  lights2[counter2].classList.add("on");
+  setTimeout(traffic2, timers2[counter2]);
+  counter2 = (counter2 + 1) % timers2.length;
+}
